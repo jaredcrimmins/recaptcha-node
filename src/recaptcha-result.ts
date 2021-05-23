f325d9c982cb7fb4f9b9eb535c377f396ce5c758
Jared Crimmins <jared@crimmins.io>
2021-05-23 12:10:58 -0500

    fix: removed apkPackageName from RecaptchaV3Result
    Removed the apkPackageName property from RecaptchaV3Result because reCAPTCHA v3 is web-only.

--- src/recaptcha-result.ts
+++ src/recaptcha-result.ts
@@ -7,23 +7,16 @@ type RecaptchaResultCreationAttrs = {
 };
 
 export abstract class RecaptchaResult {
-  /** The package name of the app where the reCAPTCHA was solved */
-  apkPackageName?: string;
-
   /** Timestamp of the challenge load */
   challengeTimestamp?: string;
 
   errorCodes?: string[];
-
-  /** The hostname of the site where the reCAPTCHA was solved */
-  hostname?: string;
 
   success: boolean;
 
   constructor(attrs: RecaptchaResultCreationAttrs) {
     this.success = attrs.success;
     this.challengeTimestamp = attrs.challenge_ts;
-    this.hostname = attrs.hostname;
     this.errorCodes = attrs['error-codes'];
   }
 }
@@ -31,19 +24,31 @@ export abstract class RecaptchaResult {
 type RecaptchaV2ResultCreationAttrs = RecaptchaResultCreationAttrs;
 
 export class RecaptchaV2Result extends RecaptchaResult {
+  /** The package name of the app where the reCAPTCHA was solved */
+  apkPackageName?: string;
+
+  /** The hostname of the site where the reCAPTCHA was solved */
+  hostname?: string;
+
   constructor(attrs: RecaptchaV2ResultCreationAttrs) {
     super(attrs);
+
+    this.apkPackageName = attrs.apk_package_name;
   }
 }
 
 type RecaptchaV3ResultCreationAttrs = RecaptchaResultCreationAttrs & {
   action: string;
+  hostname: string;
   score: number;
 };
 
 export class RecaptchaV3Result extends RecaptchaResult {
   /** The score for this request (0.0 - 1.0) */
   action: string;
+
+  /** The hostname of the site where the reCAPTCHA was solved */
+  hostname: string;
 
   /** The action name for this request */
   score: number;
@@ -52,6 +57,7 @@ export class RecaptchaV3Result extends RecaptchaResult {
     super(attrs);
 
     this.action = attrs.action;
+    this.hostname = attrs.hostname;
     this.score = attrs.score;
   }
 }
